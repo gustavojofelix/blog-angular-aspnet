1dad403adffb9b9585c5bc4f623789fec899bc55
Gustavo Felix <gustavo.felix@alvorsoftware.com>
2024-09-12 20:33:51 +0200

    Upload image API and UI initial

---
+++ API/CodePulse.API/CodePulse.API/Repositories/Implementation/ImageRepository.cs
@@ -0,0 +1,39 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly ApplicationDbContext dbContext;
+
+        public ImageRepository(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+            this.dbContext = dbContext;
+        }
+
+
+        public async Task<BlogImage> Upload(IFormFile file, BlogImage blogImage)
+        {
+            // 1- Upload the image to API/Images
+            var localPath = Path.Combine(webHostEnvironment.ContentRootPath, "Images", $"{blogImage.FileName}{blogImage.FileExtension}");
+            using var stream = new FileStream(localPath, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            // 2- Update the database
+            var request = httpContextAccessor.HttpContext.Request;
+            var urlPath = $"{request.Scheme}://{request.Host}{request.PathBase}/Images/{blogImage.FileName}{blogImage.FileExtension}";
+
+            blogImage.Url = urlPath;
+
+            await dbContext.AddAsync(blogImage);
+            await dbContext.SaveChangesAsync();
+            return blogImage;
+        }
+    }
+}
--- UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
-  styleUrls: ['./image-selector.component.css']
+  styleUrls: ['./image-selector.component.css'],
 })
 export class ImageSelectorComponent {
+  private file?: File;
+  fileName: string = '';
+  title: string = '';
 
+  constructor(private imageService: ImageService) {}
+
+  onFileUploadChange(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    this.file = element.files?.[0];
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName !== '' && this.title !== '') {
+      this.imageService
+        .uploadImage(this.file, this.fileName, this.title)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+          },
+        });
+    }
+  }
 }
